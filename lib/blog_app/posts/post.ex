f3d12fbb9abecbc58f719f882ce42b1a8c62cc28
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2022-03-17 15:32:08 -0400

    first commit

---
+++ lib/blog_app/posts/post.ex
@@ -0,0 +1,26 @@
+defmodule BlogApp.Posts.Post do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias BlogApp.Comments
+
+  def add_comment(post_id, comment_params) do
+    comment_params
+    |> Map.put("post_id", post_id)
+    |> Comments.create_comment()
+  end
+
+  schema "posts" do
+    field :body, :string
+    field :title, :string
+    has_many :comments, Comment
+    timestamps()
+  end
+
+  @doc false
+  def changeset(post, attrs) do
+    post
+    |> cast(attrs, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
